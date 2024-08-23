3e765b627a229caa9240ad58fe23b96289a1e66f
Przemyslaw Wasilewski <p.wasilewski@yetiforce.com>
2024-08-23 17:15:27 +0200

    Add messages throttling

---
+++ app/src/NotificationPublisher/Domain/Exception/MessageSendLimitExceed.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\NotificationPublisher\Domain\Exception;
+
+use Exception;
+
+class MessageSendLimitExceed extends Exception
+{
+    public const string MESSAGE = 'Message send limit %d exceed! Retry after: %d minutes';
+
+    public function __construct(
+        public int $limit,
+        public float $retryAfter,
+    ) {
+        parent::__construct(sprintf(self::MESSAGE, $limit, $retryAfter));
+    }
+}
